452cd3ded39297b77f54ef59c6da687097a099fd
rizal <rizal@rizals-MacBook-Pro.local>
2022-05-25 03:54:02 +0700

    final touch

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,8 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+echo "======================================"
 echo "LORATECH: YOUR PROJECT IS READY TO GO!"
+echo "======================================"
 
 gunicorn --bind :8000 --workers=3 admin.wsgi:application
